e7dabb5059095624f8d2324b71c6345eadd95860
iccode17 <iccode17@gmail.com>
2025-03-22 23:54:41 +0100

    switch to uv

--- src/inspectorgadget/__init__.py
+++ src/inspectorgadget/__init__.py
@@ -1,7 +1,7 @@
 #
 # MIT License
 #
-# Copyright (c) 2023 nbiotcloud
+# Copyright (c) 2023-2025 nbiotcloud
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -24,10 +24,7 @@
 """
 Inspector Gadget - Extended Python Inspection.
 
-This module extends the python built-in :any:`inspect` module
-
-* :any:`get_signature` - Get Signature as a string
-* :any:`getsize` - Sum Size of `obj` and Members
+This module extends the python built-in :any:`inspect` module.
 """
 
 import inspect as _inspect
@@ -36,58 +33,58 @@ from gc import get_referents
 from types import FunctionType, ModuleType
 
 
-def get_signature(func, vars_=None, exclude=None):
+def get_signature(func, vars_=None, exclude=None):  # noqa: C901, PLR0912
     """
     Return signature of `func` as string.
 
-    >>> def func(a, b=2):
-    ...     return a + b
-    >>> get_signature(func)
-    'func(a, b=2)'
+        >>> def func(a, b=2):
+        ...     return a + b
+        >>> get_signature(func)
+        'func(a, b=2)'
 
     In case of given variables `vars_` the caller equivalent is returned.
 
-    >>> get_signature(func, {'a': 4, 'b': 2})
-    'func(4)'
-    >>> get_signature(func, {'a': 4, 'b': 9})
-    'func(4, b=9)'
+        >>> get_signature(func, {'a': 4, 'b': 2})
+        'func(4)'
+        >>> get_signature(func, {'a': 4, 'b': 9})
+        'func(4, b=9)'
 
     This is useful in combination with `vars()`.
 
-    >>> def func(a, b=2):
-    ...     print(get_signature(func, vars()))
-    >>> func(10, b=8)
-    func(10, b=8)
+        >>> def func(a, b=2):
+        ...     print(get_signature(func, vars()))
+        >>> func(10, b=8)
+        func(10, b=8)
 
     Positional arguments:
 
-    >>> def func(a, *b):
-    ...     print(get_signature(func, vars()))
-    >>> func("a", "b0", "b1")
-    func('a', 'b0', 'b1')
+        >>> def func(a, *b):
+        ...     print(get_signature(func, vars()))
+        >>> func("a", "b0", "b1")
+        func('a', 'b0', 'b1')
 
     Keyword arguments (Please note, that the kwargs are sorted by name):
 
-    >>> def func(a, **kwargs):
-    ...     print(get_signature(func, vars()))
-    >>> func("a", foo=4, bar=8)
-    func('a', bar=8, foo=4)
+        >>> def func(a, **kwargs):
+        ...     print(get_signature(func, vars()))
+        >>> func("a", foo=4, bar=8)
+        func('a', bar=8, foo=4)
 
     Parameters can be hidden via `exclude` by name or index.
 
-    >>> def func(a, b, c, d=None, e=None):
-    ...     print(get_signature(func, vars(), exclude=(0, "d", "g")))
-    >>> func('a', 'b1', 'c2', 'd3', 'e4')
-    func('b1', 'c2', e='e4')
+        >>> def func(a, b, c, d=None, e=None):
+        ...     print(get_signature(func, vars(), exclude=(0, "d", "g")))
+        >>> func('a', 'b1', 'c2', 'd3', 'e4')
+        func('b1', 'c2', e='e4')
     """
     # pylint: disable=too-many-nested-blocks,too-many-branches
     sig = _inspect.signature(func)
     name = func.__name__
     if vars_ is None:
-        return f"{name}{str(sig)}"
+        return f"{name}{sig!s}"
     args = []
     if exclude is None:
-        exclude = tuple()
+        exclude = ()
     for idx, param in enumerate(sig.parameters.values()):
         if param.name in exclude or idx in exclude:
             continue
@@ -98,7 +95,7 @@ def get_signature(func, vars_=None, exclude=None):
                 for k, value in sorted(vars_[param.name].items()):
                     if k in exclude:  # pragma: no cover
                         continue
-                    args.append(f"{str(k)}={value!r}")
+                    args.append(f"{k!s}={value!r}")
             else:
                 args.append(repr(vars_[param.name]))
         else:
@@ -116,21 +113,21 @@ def getsize(obj, blacklist=(type, ModuleType, FunctionType)):
     """
     Sum Size of `obj` and Members.
 
-    >>> import sys
-    >>> sys.getsizeof('a')  # doctest: +SKIP
-    50
-    >>> sys.getsizeof('b')  # doctest: +SKIP
-    50
-    >>> sys.getsizeof(('a', 'b'))  # doctest: +SKIP
-    56
-    >>> getsize(('a', 'b'))  # doctest: +SKIP
-    156
-    >>> def myfunc(a):
-    ...     return a
-    >>> getsize(myfunc)
-    Traceback (most recent call last):
-      ...
-    TypeError: getsize() does not take argument of type: <class 'function'>
+        >>> import sys
+        >>> sys.getsizeof('a')  # doctest: +SKIP
+        50
+        >>> sys.getsizeof('b')  # doctest: +SKIP
+        50
+        >>> sys.getsizeof(('a', 'b'))  # doctest: +SKIP
+        56
+        >>> getsize(('a', 'b'))  # doctest: +SKIP
+        156
+        >>> def myfunc(a):
+        ...     return a
+        >>> getsize(myfunc)
+        Traceback (most recent call last):
+        ...
+        TypeError: getsize() does not take argument of type: <class 'function'>
     """
     if isinstance(obj, blacklist):
         raise TypeError(f"getsize() does not take argument of type: {type(obj)}")
